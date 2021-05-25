5372735973b8fe5779fbf1c77b3c1cef2e2f0e9d
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-05-25 10:33:41 +0200

    filter selector stage 2 done

--- src/js/script.js
+++ src/js/script.js
@@ -49,10 +49,65 @@ function initActions(){
         const indexOfInput = filters.indexOf(inputValue);
         filters.splice(indexOfInput, 1);
       }
+
+      filterBooks();
     }
-    console.log('filters', filters);
+    // console.log('filters', filters);
   });
 }
+
+function filterBooks(){
+  
+  for(let bookData of app.books){
+    const bookHTML = booksList.querySelector(`.book__image[data-id="${bookData.id}"]`);
+    let shouldBeHidden = false;
+
+    for(const filter of filters){
+      if(!bookData.details[filter] == true){
+        shouldBeHidden = true;
+        console.log('filter', filter);
+        break;
+      }
+    }
+
+    if(shouldBeHidden == true){
+      if(!bookHTML.classList.contains('hidden')){
+        bookHTML.classList.add('hidden');
+      }
+    }else if(bookHTML.classList.contains('hidden')){
+      bookHTML.classList.remove('hidden');
+    }
+  }
+}
+
+// function filterBooks(){  RAFAŁ
+//   // filters
+//   // app.books
+//   for(let bookData of app.books){
+//     console.log('bookData', bookData);
+//     const bookHTML = booksList.querySelector(`.book__image[data-id="${bookData.id}"]`);
+//     console.log('bookHTML', bookHTML);
+
+//     let isAdultOn = false;
+//     let isNonFictionOn = false;
+
+//     if(bookData.details.adults == true && filters.includes('adults')){      
+//       isAdultOn = true;
+//     }
+    
+//     if(bookData.details.nonFiction == true && filters.includes('nonFiction')){
+//       isNonFictionOn = true;
+//     }
+
+//     if(isAdultOn || isNonFictionOn){
+//       if(!bookHTML.classList.contains('hidden')){
+//         bookHTML.classList.add('hidden');
+//       }
+//     } else if(bookHTML.classList.contains('hidden')){
+//       bookHTML.classList.remove('hidden');  
+//     }
+//   }
+// }
 
 const filters = [];
 const filtersSelector = document.querySelector('.filters');
@@ -62,6 +117,7 @@ const app = {
     this.initData();
     renderBooks(this.books);
     initActions();
+    filterBooks();
   },
   initData: function(){
     this.books = dataSource.books;
